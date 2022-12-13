dbb1248d61cb9905934b942c95d5e76f29886d35
Scott Carpenter <scottcarpenter28@gmail.com>
2022-12-12 19:39:05 -0500

    Add basic game functionality

--- application/server/templates/js/index.js
+++ application/server/templates/js/index.js
@@ -6,6 +6,12 @@ class GameTile{
         this.tile_value = ""
         $("#" + parent_row_id).append("<button id='"+ tile_id +"'class='guess-tile' disabled></button>")
     }
+
+    update_tile(){
+        // Update the value of the tile
+        $("#" + this.tile_id).text(this.tile_value);
+        $("#" + this.tile_id).value = this.tile_value;
+    }
 }
 
 class GameRow{
@@ -21,6 +27,26 @@ class GameRow{
             this.tiles.push(new GameTile(this.row_id, "tile_"+ i + "_" + this.row_id))
         }
     }
+
+    set_tile_value(letter){
+        // Set the value of the next available tile
+        if(this.current_population >= this.tile_count)
+            return console.error("Row is full");
+        this.tiles[this.current_population].tile_value = letter;
+        this.tiles[this.current_population].update_tile();
+        this.current_population++;
+    }
+
+    remove_last_value(){
+        // Remove the last value in the row
+        this.current_population --;
+        if(this.current_population < 0){
+            this.current_population = 0;
+            return console.error("Row is empty");
+        }
+        this.tiles[this.current_population].tile_value = "";
+        this.tiles[this.current_population].update_tile();
+    }
 }
 
 class GameBoard{
@@ -33,18 +59,49 @@ class GameBoard{
         }
     }
     
+    add_letter(letter){
+        if(this.current_guess >= this.max_guesses)
+            return console.error("Game is over");
+
+        let current_row = this.board[this.current_guess];
+        if(current_row.current_population >= current_row.tile_count)
+            return console.error("Row is full");
+
+        current_row.set_tile_value(letter);
+    }
+
+    delete_letter(){
+        // Delete the last letter
+        if(this.current_guess >= this.max_guesses)
+            return console.error("Game is over");
+
+        let current_row = this.board[this.current_guess];
+        current_row.remove_last_value();
+    }
+
+    submit_guess(){
+        // Submit the current guess
+        if(this.current_guess >= this.max_guesses)
+            return console.error("Game is over");
+
+        this.current_guess++;
+    }
 }
 
 function button_click(event) {
     button_value = event.currentTarget.value
-    if(button_value == "ENTER" || button_value == "DELETE") {
-        return
-    }
-    console.log(button_value)
+    if(button_value == "ENTER")
+        main_game.submit_guess();
+    else if(button_value == "DELETE") 
+        main_game.delete_letter();
+    else
+        main_game.add_letter(button_value);
 }
 
-$( document ).ready(function() {
+let main_game;
+function startup(){
     $("button").on("click", button_click)
-    console.log( "ready!" );
-    test = new GameBoard();
-});
+    main_game = new GameBoard();
+}
+
+$( document ).ready(startup);
